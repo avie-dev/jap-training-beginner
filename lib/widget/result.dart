cedadcbe0d68b69ccd88a2bf74c13528c4935792
jonavilcello <avie.dev.prg@gmail.com>
2021-03-22 06:37:10 +0900

    style result page

--- lib/widget/result.dart
+++ lib/widget/result.dart
@@ -15,7 +15,7 @@ class Result extends StatelessWidget {
     } else if (resultScore >= 5) {
       resultText = 'Good! Just one more try!';
     } else if (resultScore >= 1) {
-      resultText = 'Better luck next time!';
+      resultText = 'It will be better next time!';
     } else {
       resultText = 'It\'s okay! Try again';
     }
@@ -33,27 +33,46 @@ class Result extends StatelessWidget {
             borderRadius: BorderRadius.circular(15.0),
           ),
           elevation: 10,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  resultPhrase,
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                Text(resultScore.toString()),
-                TextButton(
-                  child: Text(
-                    'Restart Quiz!',
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/bg/colorfulbg.gif"),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    resultPhrase,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
                   ),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
+                  SizedBox(
+                    height: 30,
                   ),
-                  onPressed: resetHandler,
-                ),
-              ],
+                  Text(
+                    resultScore.toString(),
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  TextButton(
+                    child: Text(
+                      'Restart Quiz!',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: resetHandler,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
